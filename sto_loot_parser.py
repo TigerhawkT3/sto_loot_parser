d3f0c45e23e4e78d6ec21904aff69ca3b16e9b22
David Muller <david@mullerhome.com>
2016-05-11 22:39:03 -0700

    Add many analysis methods to Container, add to expression, add testing calls, refactor Loot attributes.

--- sto_loot_parser.py
+++ sto_loot_parser.py
@@ -1,8 +1,11 @@
 import re
 import datetime
 import sys
+import collections
 
-year = datetime.datetime.now().year
+now = datetime.datetime.now()
+year = now.year
+min_date = datetime.datetime(1, 1, 1)
 
 class Container:
     def __init__(self):
@@ -19,8 +22,43 @@ class Container:
     def extend(self, other):
         self.bag.extend(other.bag)
     
+    def __bool__(self):
+        return bool(self.bag)
+        
     def __iter__(self):
         return iter(self.bag)
+    
+    def get_loot(self, **filters):
+        ranges = {k:filters.pop(k) if k in filters else v
+                for k,v in (('min_date', min_date), ('max_date', now),
+                            ('min_gain', 0), ('max_gain', 10000000000),
+                            ('min_loss', 0), ('max_loss', -10000000000))}
+        for event in self:
+            if (ranges['min_date'] <= event.datetime <= ranges['max_date']
+            ) and (ranges['min_gain'] <= event.gain_value <= ranges['max_gain']
+            ) and (ranges['max_loss'] <= event.loss_value <= ranges['min_loss']) and all(
+                v in getattr(event, k) for k,v in filters.items()):
+                yield event
+                
+    def average(self, loss=False, **filters):
+        loot = [item.loss_value if loss else item.gain_value
+                for item in self.get_loot(**filters)]
+        return sum(loot)/len(loot)
+    
+    def counter(self, **filters):
+        return collections.Counter(val for item in self.get_loot(**filters)
+                                    for val in (item.loss_item, item.gain_item) if val)
+        
+    def common(self, least=False, counter=None, **filters):
+        if counter:
+            return counter.most_common()[-1*least]
+        return self.counter(**filters).most_common()[-1*least]
+    
+    def __str__(self):
+        return '\n'.join(str(item) for item in self)
+    
+    def __repr__(self):
+        return str(self)
 
 class Loot:
     def __init__(self, d, t, interaction, winner, quantity, item):
@@ -42,32 +80,43 @@ class Loot:
         
         item = item.split(' x ')[0].rstrip('!.').rsplit(' erhalten', maxsplit=1)[0]
         
-        if interaction in {'lost', 'placed a bet of'}:
-            self.gain = None
-            self.loss = (item, quantity * -1)
+        self.interaction = interaction
+        
+        if interaction in {'lost', 'placed a bet of', 'discarded'}:
+            self.gain_item = ''
+            self.gain_value = 0
+            self.loss_item = item
+            self.loss_value = quantity * -1
         elif interaction == 'sold':
             item, gain = item.rsplit(' for ', maxsplit=1)
-            self.loss = (item, -1)
+            self.loss_item = item
+            self.loss_value = -1
             quantity, item = gain.split(maxsplit=1)
-            self.gain = (item, int(quantity.replace(',', '')))
+            self.gain_item = item
+            self.gain_value = int(quantity.replace(',', ''))
         else:
-            self.gain = (item, quantity)
-            self.loss = None
+            self.gain_item = item
+            self.gain_value = quantity
+            self.loss_item = ''
+            self.loss_value = 0
     
     def __str__(self):
         if self.winner:
-            return '{} won {}.'.format(self.winner, self.gain[0])
+            return '{} won {}.'.format(self.winner, self.gain_item)
         
-        if self.gain:
-            gain = 'Gained {} x{}.'.format(*self.gain)
+        if self.gain_item:
+            gain = 'Gained {} x{}.'.format(self.gain_item, self.gain_value)
         else:
             gain = 'No gains.'
-        if self.loss:
-            loss = 'Lost {} x{}.'.format(self.loss[0], self.loss[1]*-1)
+        if self.loss_item:
+            loss = 'Lost {} x{}.'.format(self.loss_item, self.loss_value*-1)
         else:
             loss = 'No losses.'
         
         return '{}: {} {}'.format(self.datetime, gain, loss)
+    
+    def __repr__(self):
+        return str(self)
 
 s = '''[3/19 12:41] [System] [ItemReceived] Items acquired: Astrometric Probes x 10
 [3/19 12:41] [System] [ItemReceived] Items acquired: Astrometric Probes x 10
@@ -99,7 +148,7 @@ s = '''[3/19 12:41] [System] [ItemReceived] Items acquired: Astrometric Probes x
     
 expression = (r'^(?:\[(\d+/\d+)? ?(\d+:\d+)?\] )?(?:\[[^]]+\] )?' +
               r'(?:\[(?:NumericReceived|ItemReceived|NumericLost|GameplayAnnounce|Default)\] )?' +
-              r'(?:You (lost|received|sold|placed a bet of|won)|Items? acquired:|(.*) '
+              r'(?:You (discarded|lost|refined|received|sold|placed a bet of|won)|Items? acquired:|(.*) '
               r'(?:has acquired|hat einen))' +
               r' ([0-9,]+ )?(.*)')
 
@@ -107,12 +156,22 @@ container = Container()
 for loot in (Loot(*match.groups()) for match in (re.match(expression, line) for line in s.split('\n')) if match):
     container.add(loot)
 
-for match in container.bag:
+for match in container.get_loot(gain_item='Energy Credits', min_date=datetime.datetime(2016,4,1)):
     print(match)
-    
-    
-    
-    
-    
-    
-    
+
+print(container.average(loss=True, gain_item='Energy Credits', min_date=datetime.datetime(2016,4,1)))
+
+count = container.counter()
+
+print(count)
+
+print(container.common(counter=count, least=True))
+
+
+
+
+
+
+
+
+
